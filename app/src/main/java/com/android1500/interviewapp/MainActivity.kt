4ae3aef3b90eb6f5cb943fe92928a828ac610827
Android1500 <yuviyboy1500@gmail.com>
2022-10-03 04:00:23 -0700

    Clean code and add more navigation component
    -Add MaterialSharedAxis for enter and exit animation while changing fragment
    
    - Add Toolbar update with current fragment
    
    - Add Toolbar navigation back

--- app/src/main/java/com/android1500/interviewapp/MainActivity.kt
+++ app/src/main/java/com/android1500/interviewapp/MainActivity.kt
@@ -6,8 +6,13 @@ import android.os.Bundle
 import android.provider.Settings
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.core.content.res.ResourcesCompat
+import androidx.core.view.WindowCompat
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.*
 import com.android1500.interviewapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -15,12 +20,20 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window,false)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
         binding.permissionAccessibility.setOnClickListener {
             startActivity(Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS))
         }
@@ -28,9 +41,24 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_about -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 
 
-
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.fragmentContainerView)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
 
 
 }
--- app/src/main/java/com/android1500/interviewapp/ui/base/BaseFragment.kt
+++ app/src/main/java/com/android1500/interviewapp/ui/base/BaseFragment.kt
@@ -1,11 +1,18 @@
 package com.android1500.interviewapp.ui.base
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
+import com.android1500.interviewapp.R
+import com.google.android.material.transition.FadeThroughProvider
+import com.google.android.material.transition.MaterialSharedAxis
+import com.google.android.material.transition.SlideDistanceProvider
+import kotlin.math.roundToInt
 
 abstract class BaseFragment<T : ViewBinding>(private val inflate: (LayoutInflater, ViewGroup?, Boolean) -> T) :
     Fragment() {
@@ -14,6 +21,13 @@ abstract class BaseFragment<T : ViewBinding>(private val inflate: (LayoutInflate
     protected val binding: T
         get() = _binding ?: throw RuntimeException("Cannot access binding before onCreateView or after onDestroyView")
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        exitTransition = getMaterialSharedAxis(requireContext(), true)
+        enterTransition = getMaterialSharedAxis(requireContext(), true)
+        returnTransition = getMaterialSharedAxis(requireContext(), false)
+        reenterTransition = getMaterialSharedAxis(requireContext(), false)
+    }
 
 
     override fun onCreateView(
@@ -29,5 +43,16 @@ abstract class BaseFragment<T : ViewBinding>(private val inflate: (LayoutInflate
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+
+    private fun getMaterialSharedAxis(context: Context, forward: Boolean): MaterialSharedAxis {
+        return MaterialSharedAxis(MaterialSharedAxis.X, forward).apply {
+            (primaryAnimatorProvider as SlideDistanceProvider).slideDistance =
+                context.resources.getDimension(R.dimen.shared_axis_x_slide_distance).roundToInt()
+            duration = 450L
+            (secondaryAnimatorProvider as FadeThroughProvider).progressThreshold = 0.22f
+            interpolator = AnimationUtils.loadInterpolator(context, R.anim.fast_out_extra_slow_in)
+        }
     }
 }
